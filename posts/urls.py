3f0409a778813eee445274334fce4076b8607a3d
Ghazi AlSanie <alsanie.g@gmail.com>
2017-11-22 19:20:51 +0300

    all week 4 coding

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,14 @@ urlpatterns = [
     url(r'^create/$', views.post_create, name="create"),
     url(r'^detail/(?P<post_slug>[-\w]+)/$', views.post_detail, name="detail"),
     url(r'^list/$', views.post_list, name="list"),
+    
     url(r'^update/(?P<post_slug>[-\w]+)/$', views.post_update, name="update"),
-    url(r'^delete/(?P<post_slug>[-\w]+)/$', views.post_delete, name="delete")
+    url(r'^delete/(?P<post_slug>[-\w]+)/$', views.post_delete, name="delete"),
+    
+    url(r'^like_button/(?P<post_slug>[-\w]+)/$', views.like_button, name="like_button"),
+
+    url(r'^signup/$', views.usersignup, name="usersignup"),
+    url(r'^login/$', views.userlogin, name="userlogin"),
+    url(r'^logout/$', views.userlogout, name="userlogout"),
     ]
   
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
-from .forms import PostForm
+from .models import Post, Like
+from .forms import PostForm, UserSignUp, UserLogin
 from django.contrib import messages 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from urllib.parse import quote
-from django.http import Http404
+from django.http import Http404, JsonResponse
+from django.utils import timezone
+from django.db.models import Q 
+from django.contrib.auth import login, logout, authenticate
 
 
 def post_update(request, post_slug):
@@ -26,8 +29,25 @@ def post_update(request, post_slug):
 	return render(request, 'post_update.html', context)
 
 def post_list(request):
-	objects = Post.objects.all()#.order_by('title') <==this could be added here in case you want to ovverride the Model ordering
-	paginator = Paginator(objects, 2)
+	today = timezone.now().date()
+	
+	objects = Post.objects.filter(draft=False, publish__lte=today)#.order_by('title') <==this could be added here in case you want to ovverride the Model ordering
+	
+	if request.user.is_staff:
+		objects= Post.objects.all()
+
+	query = request.GET.get('q')
+	if query:
+		objects = objects.filter(
+		Q(title__icontains=query)|
+		Q(content__icontains=query)|
+		Q(author__first_name__icontains=query)|
+		Q(author__last_name__icontains=query)
+		).distinct()
+
+
+
+	paginator = Paginator(objects, 6)
 	number = request.GET.get ('page')
 	try:
 		objects = paginator.page(number)
@@ -37,13 +57,30 @@ def post_list(request):
 		objects = paginator.page(paginator.num_pages)
 	context = {
 		"post_items": objects,
+		"today": today,
 	}
+	
 	return render(request, "post_list.html", context)
 
 def post_detail(request, post_slug):
 	item = get_object_or_404 (Post, slug=post_slug)
+	if not request.user.is_staff:
+		if item.draft or item.publish > timezone.now().date():
+			raise Http404
+	liked = False
+	if request.user.is_authenticated():
+		if Like.objects.filter(post=item, user=request.user).exists():
+			liked = True 
+		else:
+			liked = False
+
+	like_count = item.like_set.count()
+
+
 	context = {
 		"item": item,
+		"liked": liked,
+		"like_count": like_count,
 	}
 	return render(request, "post_detail.html", context)
 
@@ -52,7 +89,9 @@ def post_create(request):
 		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
-		form.save()
+		item = form.save(commit=False)
+		item.authour = request.user
+		item.save()
 		messages.success(request, "Awesome, you just added a blog post!")
 		return redirect("posts:list")
 	context = {
@@ -66,6 +105,77 @@ def post_delete(request, post_slug):
 	Post.objects.get(slug=post_slug).delete()
 	messages.warning(request, "All cleaned up! Post deleted!")
 	return redirect("posts:list")
+
+def like_button(request, post_slug):
+	post_object = Post.objects.get(slug=post_slug)
+
+	like, created = Like.objects.get_or_create(user=request.user, post=post_object)
+
+	if created:
+		action = "like"
+	else:
+		like.delete()
+		action = "unlike"
+
+	like_count = post_object.like_set.count()
+
+	response = {
+
+		"action": action,
+		"like_count": like_count,
+
+
+	}
+
+	return JsonResponse(response, safe=False)
+
+def usersignup(request):
+    context = {}
+    form = UserSignup()
+    context['form'] = form
+
+    if request.method == "POST":
+        form = UserSignup(request.POST)
+        if form.is_valid():
+            user = form.save()
+            x = user.username
+            y = user.password
+            user.set_password(y)
+            user.save()
+            auth = authenticate(username=x, password=y)
+            login(request, auth)
+            return redirect("posts:list")
+        messages.warning(request, form.errors)
+        return redirect("posts:signup")
+    return render(request, 'signup.html', context)
+
+def userlogin(request):
+    context = {}
+    form = UserLogin()
+    context['form'] = form
+    if request.method == "POST":
+        form = UserLogin(request.POST)
+        if form.is_valid():
+            some_username = form.cleaned_data['username']
+            some_password = form.cleaned_data['password']
+            auth = authenticate(username=some_username, password=some_password)
+            if auth is not None:
+                login(request, auth)
+                return redirect("posts:list")
+            messages.warning(request, 'Incorrect Username/Password combination. *cough cough* noob.')
+            return redirect("posts:login")
+        messages.warning(request, form.errors)
+        return redirect("posts:login")
+    return render(request, 'login.html', context)
+
+def userlogout(request):
+    logout(request)
+    return redirect("posts:login")
+
+
+
+
+
 
 
 # Create your views here.
